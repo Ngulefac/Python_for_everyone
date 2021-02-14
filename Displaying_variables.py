66f518da263a6ef37834b033e8254ac340ec419d
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:07:47 +0100

    Output : My name is Theodore and I am 19 years old

---
+++ Displaying_variables.py
@@ -0,0 +1,6 @@
+name = "Theodore"
+age = int(19)
+# python takes care of the data type
+# in otherwords, you dont have to specify the data type during declaration
+print("My name is",name,"and I am",age,"years old \n") 
+# the '\n' simply stands for a new line
