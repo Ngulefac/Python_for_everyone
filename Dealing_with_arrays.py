0dd1dec9c9e10865dd10b528681b91bc030bbfc1
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:41:35 +0100

    Added a new line

--- Dealing_with_arrays.py
+++ Dealing_with_arrays.py
@@ -3,3 +3,11 @@ print(list) # prints out the whole array
 print(list[0:3]) # prints out from index 0 to index 2 
 print(list[2:])  # prints out from index 2 till the end of the list
 print(list[:4]) #prints out from index 0 till the fourth element (index 3)
+print(list[:])  #prints everything from the first index (index 0 till the last element)
+#Rather than using 'print(list[:])' use 'print(list)' as they both do thesame thing with the later being the
+#simplified one
+
+# 2D arrays
+
+
+
