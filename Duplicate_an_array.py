1b34bc7ac52973c278c6f592438c2d834dc6b3a6
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:50:39 +0100

    Created the new array y and displayed it's contents to make sure everything was copied

---
+++ Duplicate_an_array.py
@@ -0,0 +1,4 @@
+# There are times when you probably want to get a copy of an existing array for fear of data lost or other perculiear reasons
+x = ["one","two","three","four","five","six"]
+y =[x]
+print(y) # To make sure that the array ,y, is valid
