ad66079675df81b4d5ec1f9b1a8e7a65d97b2c21
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:45:37 +0100

    Explannation of the 2 dimensional array

--- Dealing_with_arrays.py
+++ Dealing_with_arrays.py
@@ -10,6 +10,11 @@ print(list[:])  #prints everything from the first index (index 0 till the last e
 # 2D arrays
 nestlist = [1,2,[10,20,30,[7,9,11,[100,200,300]]],[1,7,8]]
 print(nestlist[2][3]) #prints out the index 3 of the index 2 position in nestlist
+print(nestlist[2][3][0]) #prints out the index 0 of index 3 of the index 2 position in nestlist
+# in other words 
+# print(nestlist[2][3][0]) means
+# get the index 2 element,inside index 2 get the index 3 element, inside index 3 get the first element ,index 0
+# print it out
 
 
 
