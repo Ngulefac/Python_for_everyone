443af3e1005c7253a10565a3a2592b2ef506d0fc
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 06:31:43 +0100

    Joined the arrays

---
+++ Adding_arrays_using_the_extend_function.py
@@ -0,0 +1,4 @@
+print([2,3,2,2,] + [24,24,3,535,53,53])localhost@kali:/media/localhost/███████████████/Backup/Desktop/Cloned Repo For My Profile/Python Samples$ cat Ex23.py
+a = [1, 2, 3]
+c = 4,5,6,7,8,9
+a.extend(c)
