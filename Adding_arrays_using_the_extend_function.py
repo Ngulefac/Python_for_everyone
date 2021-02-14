a5ffc1e08b48668938d8dd752d4ed52086b45990
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 06:32:36 +0100

    Displayed the joined array

--- Adding_arrays_using_the_extend_function.py
+++ Adding_arrays_using_the_extend_function.py
@@ -1,4 +1,5 @@
 print([2,3,2,2,] + [24,24,3,535,53,53])localhost@kali:/media/localhost/███████████████/Backup/Desktop/Cloned Repo For My Profile/Python Samples$ cat Ex23.py
 a = [1, 2, 3]
 c = 4,5,6,7,8,9
-a.extend(c)
+a.extend(c) # c is added to a 
+print(a)
