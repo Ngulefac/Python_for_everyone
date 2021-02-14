0b06b97689eca7350905114964eace4f7b783898
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:19:38 +0100

    Addition,Multiplication,Division and Average

--- Basic_math_operation.py
+++ Basic_math_operation.py
@@ -7,3 +7,6 @@ num4 = num1 + num2 + num3
 num5 = num1 * num2 * num3
 #Division with num1 and num 3
 num6 = num3 / num3
+#Average
+num7 = num4/3
+# I am using num4 because it's the total sum [*] Average = total sum/Number of occurances
