56fc9818e01e08d2cec56da780cdd3e7e236f727
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:54:49 +0100

    Deleted from indedx 2 till index 4 inclusive

---
+++ Deleting_element(s)_in_an_array.py
@@ -0,0 +1,5 @@
+# There are times when you probably need to remove some stuffs from a list
+x = ["one","two","three","four","five","six"]
+print(x) #Making sure everything is printed 
+del x[2:5] # deleting from index 2 till index 4
+print(x) #Making sure the deletion was successful
