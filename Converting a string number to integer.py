9765851281630e8c50492a74f16bf27fc3caf8cd
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:33:52 +0100

    String-integer conversion

---
+++ Converting a string number to integer.py
@@ -0,0 +1,5 @@
+a = 7
+b = "8"  # Here the value of b is a sting not an integer
+z = a + int(b) #for this addition to occur you need to convert this string ,b to its corresponding integer
+print(z)
+#z = a + b   [*] >> Will result to an error
