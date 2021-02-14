a97b644c6cde2841ffbb3da0cb3e6b6a81c93380
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 06:39:35 +0100

    Updated the file

--- Checking_if_an_element_is_in_an_an_array.py
+++ Checking_if_an_element_is_in_an_an_array.py
@@ -1,5 +1,7 @@
 list = 0,2,1,1,2,4,3,9,6 # This is an altenate way of declaring an array
 print(1 in list)
 print(7 in  list)
+print(9-4 in  list)
+print(9-4 not in  list)
 print([list[2]] + [list[1]])
 # done
