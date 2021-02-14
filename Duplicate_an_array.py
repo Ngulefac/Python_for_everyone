8792af9a30164c164b6d4a49b9807d00b560175c
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:51:39 +0100

    Keeping things clean

--- Duplicate_an_array.py
+++ Duplicate_an_array.py
@@ -1,4 +1,6 @@
 # There are times when you probably want to get a copy of an existing array for fear of data lost or other perculiear reasons
 x = ["one","two","three","four","five","six"]
 y =[x]
+z = x
 print(y) # To make sure that the array ,y, is valid
+print(z) # Take note of the difference between y and z
