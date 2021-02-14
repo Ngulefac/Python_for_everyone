cc31b56497bc90b4abbfb7c55cd6a027a2386ac2
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:26:52 +0100

    Displaying all the values

--- Basic_math_operation.py
+++ Basic_math_operation.py
@@ -6,7 +6,12 @@ num4 = num1 + num2 + num3
 #Multiplication
 num5 = num1 * num2 * num3
 #Division with num1 and num 3
-num6 = num3 / num3
+num6 = num3 / num1
 #Average
 num7 = num4/3
 # I am using num4 because it's the total sum [*] Average = total sum/Number of occurances
+# #With these operators you can do literally any given mathematical calculations
+
+#To print out this values
+print("Num1 = ",num1,"\nNum2 = ",num2,"\nNum3 = ",num3,"\nSum total = ",num4,"\nTheir products = ",num5)
+print("\nNum3 / Num1 = ",num6,"\nTotal Average = ",num7)
