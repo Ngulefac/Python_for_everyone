5f76d96004cbfeac00d8d567da8e048d4cdee4e8
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:38:50 +0100

    Added a new line

--- Dealing_with_arrays.py
+++ Dealing_with_arrays.py
@@ -2,3 +2,4 @@ list = [1,2,3,4,5,6,7]
 print(list) # prints out the whole array
 print(list[0:3]) # prints out from index 0 to index 2 
 print(list[2:])  # prints out from index 2 till the end of the list
+print(list[:4]) #prints out from index 0 till the fourth element (index 3)
