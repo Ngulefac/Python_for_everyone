91a180c9931dcf63d2e19e45cbff6ac6c96495dc
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 06:24:12 +0100

    Added the interactive part using print statements

--- Receiving_data_from_the_user.py
+++ Receiving_data_from_the_user.py
@@ -3,3 +3,5 @@ name = input("Hello what is you name: ")
 # # recieving an integer
 age = int(input("How old are you: ")) #take note that I have specified the data type
 myAge = 90
+print("Hello",name,"I am",myAge,"years old")
+print("And I am",myAge - age,"years older than you ... :-)")
