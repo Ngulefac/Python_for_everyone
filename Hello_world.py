e9fcd49a5cc807ab5b154bd4dd60c42ed4ea8681
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:00:27 +0100

    Added a professional/altenative way to displaay a text using the 'import sys' lib

--- Hello_world.py
+++ Hello_world.py
@@ -1,4 +1,6 @@
 # Welcome to my python course
 # This is how you write a comment in python
+import sys
 print("Hello world!") #using double quotes
 print('Hello world!') #using single quotes
+sys.stdout.write("Hello world!")
