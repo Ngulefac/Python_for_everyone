445610046117570029d6ac3cbe1f3e9eef6b5642
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 04:58:56 +0100

    Altenative ways to display a text

--- Hello_world.py
+++ Hello_world.py
@@ -1,5 +1,4 @@
 # Welcome to my python course
 # This is how you write a comment in python
-print("Hello world!")
-
-
+print("Hello world!") #using double quotes
+print('Hello world!') #using single quotes
