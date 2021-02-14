8bb324c8f75d0839411f8794713eef070b0a18f1
Ngulefac <ngulefacfolefac@gmail.com>
2021-02-14 05:11:07 +0100

    Take note of the difference betwwen the two formats

--- Displaying_variables.py
+++ Displaying_variables.py
@@ -3,4 +3,6 @@ age = int(19)
 # python takes care of the data type
 # in otherwords, you dont have to specify the data type during declaration
 print("My name is",name,"and I am",age,"years old \n") 
-# the '\n' simply stands for a new line
+# the '\n' simply stands for a new 
+# you can also use the '+' when concatenating two strings
+print("My name is "+name+" and I am",age,"years old \n") 
