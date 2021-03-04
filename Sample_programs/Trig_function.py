5c7af7e287e52e327a4c1c2b167f5e5f50584e20
Ngulefac <ngulefacfolefac@gmail.com>
2021-03-04 18:54:02 +0100

    Added the Tangent tables

--- Sample_programs/Trig_function.py
+++ Sample_programs/Trig_function.py
@@ -1,6 +1,9 @@
 from math import pi, sin, cos, tan
 x = {x: sin(x*pi/180) for x in range(0, 90)}
 y = {y: cos(y*pi/180) for y in range(0, 90)}
+z = tangent_table = {z: tan(z*pi/180) for z in range(0, 90)}
 
 print("sin()=", x)
 print("cos()=", y)
+print("tan()=", z)
+
