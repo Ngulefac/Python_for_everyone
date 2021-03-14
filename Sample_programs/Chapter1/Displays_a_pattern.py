f9f5903974911e4da31d2f4fe5b72931463e6282
Ngulefac <ngulefacfolefac@gmail.com>
2021-03-14 05:30:56 +0100

    added some space

--- Sample_programs/Chapter1/Displays_a_pattern.py
+++ Sample_programs/Chapter1/Displays_a_pattern.py
@@ -6,18 +6,22 @@ turtle.penup()
 turtle.goto(-200, 200)
 turtle.pendown()
 turtle.write("FFFFFFF")
+
 turtle.penup()
 turtle.goto(-200, 180)
 turtle.pendown()
 turtle.write("FF")
+
 turtle.penup()
 turtle.goto(-200, 160)
 turtle.pendown()
 turtle.write("FFFFFFF")
+
 turtle.penup()
 turtle.goto(-200, 140)
 turtle.pendown()
 turtle.write("FF")
+
 turtle.penup()
 turtle.goto(-200, 120)
 turtle.pendown()
