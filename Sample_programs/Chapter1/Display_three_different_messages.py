33f2efb451a608d375852c9caf59dd847ebdfa4f
Ngulefac <ngulefacfolefac@gmail.com>
2021-03-14 06:00:16 +0100

    Added some usefull tips about python versions

--- Sample_programs/Chapter1/Display_three_different_messages.py
+++ Sample_programs/Chapter1/Display_three_different_messages.py
@@ -4,3 +4,12 @@ print("Welcome to Computer programming in Python 3.8.2")
 print("Programming is fun")
 
 # Hey do you know how to check your version of python?
+#let me show you
+#Using Linux: Go to the terminal and type
+"""
+python3 -V   | To check the version of Python3 , because you can have Python 3.7,python 3.8 etc
+So right now the latest version is Python 3.8.2
+Python2 -V | to check the version of python2. 
+
+Using the 3 quotes (") , you can write a multi-line comment 
+"""
