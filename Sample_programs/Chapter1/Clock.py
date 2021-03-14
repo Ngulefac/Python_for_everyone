07b25b87389600a72ea449944176477626f1540d
Ngulefac <ngulefacfolefac@gmail.com>
2021-03-14 05:18:31 +0100

    Draws the minute hand

--- Sample_programs/Chapter1/Clock.py
+++ Sample_programs/Chapter1/Clock.py
@@ -34,3 +34,10 @@ turtle.penup()
 turtle.goto(-40, -180)
 turtle.pendown()
 turtle.write("9:15:00 AM")
+
+turtle.penup()
+turtle.goto(0, 25)
+turtle.goto(0, 25)
+turtle.pendown()
+turtle.forward(150)
+
