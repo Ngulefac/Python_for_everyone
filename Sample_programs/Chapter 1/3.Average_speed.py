592736508863f8aadc5b6f044beb7ec51cfe3d20
Ngulefac <ngulefacfolefac@gmail.com>
2021-03-17 02:59:02 +0100

    This program that displays the average speed in miles per hour

---
+++ Sample_programs/Chapter 1/3.Average_speed.py
@@ -0,0 +1,5 @@
+# (Note that1 mile is 1.6 kilometers.)
+# km -> miles: 14km/1.6km = 8.6992miles
+# mins -> hours: 45mins/60mins = 0.75hr
+# sec -> hours: 30secs/3600mins = 0.0083hr
+# hours= 0.833hrs
